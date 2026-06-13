400d3266b97988887fcc2f48caebca4de073f9bf
Felipe Juris Jacques <sr.jj.arrebenta@protonmail.com>
2026-06-13 18:14:22 -0300

    ajuste no nivelador e da margem

--- Mark V/Marlin-2.1.2.7/Marlin/Configuration.h
+++ Mark V/Marlin-2.1.2.7/Marlin/Configuration.h
@@ -1511,11 +1511,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -3.0, 38.0, -2.95 } // MARK V
+#define NOZZLE_TO_PROBE_OFFSET { 3.0, 38.0, -2.95 } // MARK V
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 30 // MARK V
+#define PROBING_MARGIN 20 // MARK V
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (133*60)
